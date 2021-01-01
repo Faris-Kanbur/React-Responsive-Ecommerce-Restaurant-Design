ae1f2e75c552353993d0491df7d559c3cfb6d0b4
faris <faris38kanbur@gmail.com>
2021-01-01 14:39:03 +0100

    add styled component

---
+++ restaurant/src/componnets/Navbar/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Nav, NavLink} from './NavbarElements';
+
+const Navbar = () => {
+    return (
+        <>
+           < Nav>
+             <NavLink to='/'>Pizza</NavLink>
+           </Nav>
+        </>
+    )
+}
+
+export default Navbar;
