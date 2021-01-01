c54fba9db3a576d2ae9005961fca02b90d37e10a
faris <faris38kanbur@gmail.com>
2021-01-01 17:34:06 +0100

    add hero section style

--- restaurant/src/componnets/Hero/index.js
+++ restaurant/src/componnets/Hero/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import Navbar from '../Navbar';
-import {HeroContainer} from './HeroElements';
+import {
+    HeroContainer, 
+    HeroContent,
+    HeroItems,
+    HeroH1,
+    HeroP,
+    HeroBtn
+} from './HeroElements';
 
 const Hero = () => {
     return (
         <HeroContainer>
             <Navbar />
+            <HeroContent>
+                <HeroItems>
+                    <HeroH1>Greatest Pizza Ever</HeroH1>
+                    <HeroP>Ready in 60 Second</HeroP>
+                    <HeroBtn>Place Order</HeroBtn>
+                </HeroItems>
+            </HeroContent>
         </HeroContainer>
     );
 };
