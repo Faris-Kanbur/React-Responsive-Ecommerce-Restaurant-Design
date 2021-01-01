9f0ca22d62647241832563f8fbe4110ccfa184af
faris <faris38kanbur@gmail.com>
2021-01-01 19:02:45 +0100

    add sidebar and styled

--- restaurant/src/componnets/Hero/index.js
+++ restaurant/src/componnets/Hero/index.js
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Navbar from '../Navbar';
+import Sidebar from '../Sidebar';
 import {
     HeroContainer, 
     HeroContent,
     HeroItems,
     HeroH1,
     HeroP,
-    HeroBtn
+    HeroBtn,
 } from './HeroElements';
 
 const Hero = () => {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const toggle = () => {
+        setIsOpen(!isOpen)
+    };
+
     return (
         <HeroContainer>
-            <Navbar />
+            <Navbar toggle={toggle} />
+            <Sidebar isOpen={isOpen} toggle={toggle} />
             <HeroContent>
                 <HeroItems>
                     <HeroH1>Greatest Pizza Ever</HeroH1>
